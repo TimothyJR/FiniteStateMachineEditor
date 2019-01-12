1b5e9056b5922b664a8d92b541d46bd93182e58e
Timothy Reuter <timothyjreuter@gmail.com>
2019-01-11 20:58:39 -0500

    Initial commit of project

---
+++ FiniteStateMachineEditor/FiniteStateMachineEditor/Assets/Editor/Connection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+
+/// <summary>
+/// Holds a connection between two nodes and the direction of that connection.
+/// </summary>
+public class Connection
+{
+	private ConnectionPoint inPoint;
+	private ConnectionPoint outPoint;
+	private Action<Connection> OnClickedRemoveConnection;
+
+	public ConnectionPoint InPoint
+	{ get { return inPoint; } }
+
+	public ConnectionPoint OutPoint
+	{ get { return outPoint; } }
+
+	public Connection(ConnectionPoint inPoint, ConnectionPoint outPoint, Action<Connection> OnClickedRemoveConnection)
+	{
+		this.inPoint = inPoint;
+		this.outPoint = outPoint;
+		this.OnClickedRemoveConnection = OnClickedRemoveConnection;
+	}
+
+	public void Draw()
+	{
+		Handles.DrawBezier(inPoint.Rectangle.center, outPoint.Rectangle.center, inPoint.Rectangle.center + Vector2.left * 50f, outPoint.Rectangle.center - Vector2.left * 50f, Color.white, null, 2.0f);
+
+		if(Handles.Button((inPoint.Rectangle.center + outPoint.Rectangle.center) * 0.5f, Quaternion.identity, 4, 8, Handles.RectangleHandleCap))
+		{
+			if(OnClickedRemoveConnection != null)
+			{
+				OnClickedRemoveConnection(this);
+			}
+		}
+	}
+}
---
+++ FiniteStateMachineEditor/FiniteStateMachineEditor/Assets/Editor/Node.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+/// <summary>
+/// Holds data for the editor
+/// Each node handles dragging and removing itself
+/// </summary>
+public class Node
+{
+	private Rect rectangle;
+	private string title;
+
+	private GUIStyle nodeStyle;
+	private GUIStyle defaultStyle;
+
+	private GUIStyle selectedStyle;
+
+	private bool isDragged;
+	private bool isSelected;
+
+	private ConnectionPoint inPoint;
+	private ConnectionPoint outPoint;
+
+	public Action<Node> OnRemoveNode;
+
+	public Rect Rectangle
+	{ get { return rectangle; } }
+
+	public ConnectionPoint InPoint
+	{ get { return inPoint; } }
+
+	public ConnectionPoint OutPoint
+	{ get { return outPoint; } }
+
+	public Node(Vector2 position, float width, float height, GUIStyle defaultStyle, GUIStyle selectedStyle, GUIStyle inPointStyle, GUIStyle outPointStyle, Action<ConnectionPoint> OnClickInPoint, Action<ConnectionPoint> OnClickOutPoint, Action<Node> OnClickRemoveNode)
+	{
+		rectangle = new Rect(position.x, position.y, width, height);
+		nodeStyle = defaultStyle;
+		this.defaultStyle = defaultStyle;
+		this.selectedStyle = selectedStyle;
+		inPoint = new ConnectionPoint(this, ConnectionPointType.In, inPointStyle, OnClickInPoint);
+		outPoint = new ConnectionPoint(this, ConnectionPointType.Out, outPointStyle, OnClickOutPoint);
+		OnRemoveNode = OnClickRemoveNode;
+	}
+
+	public void DragNode(Vector2 delta)
+	{
+		rectangle.position += delta;
+	}
+
+	public void Draw()
+	{
+		inPoint.Draw();
+		outPoint.Draw();
+		GUI.Box(rectangle, title, nodeStyle);
+	}
+
+	public bool ProcessEvent(Event e)
+	{
+		switch(e.type)
+		{
+			case EventType.MouseDown:
+				if(e.button == 0)
+				{
+					if(rectangle.Contains(e.mousePosition))
+					{
+						isDragged = true;
+						GUI.changed = true;
+						isSelected = true;
+						nodeStyle = selectedStyle;
+					}
+					else
+					{
+						GUI.changed = true;
+						isSelected = false;
+						nodeStyle = defaultStyle;
+					}
+				}
+				if(e.button == 1 && isSelected && rectangle.Contains(e.mousePosition))
+				{
+					ProcessContextMenu();
+					e.Use();
+				}
+				break;
+
+			case EventType.MouseUp:
+				isDragged = false;
+				break;
+			case EventType.MouseDrag:
+				if(e.button == 0 && isDragged)
+				{
+					DragNode(e.delta);
+					e.Use();
+					return true;
+				}
+				break;
+		
+
+		}
+		return false;
+	}
+
+	private void ProcessContextMenu()
+	{
+		GenericMenu genericMenu = new GenericMenu();
+		genericMenu.AddItem(new GUIContent("Remove node"), false, OnClickRemoveNode);
+		genericMenu.ShowAsContext();
+	}
+
+	private void OnClickRemoveNode()
+	{
+		if(OnRemoveNode != null)
+		{
+			OnRemoveNode(this);
+		}
+
+	}
+}
